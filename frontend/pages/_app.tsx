5497fff5e5531331b23902ba80855614eba40f8b
takurinton <takurinton@takurinton.com>
2021-10-23 00:09:41 +0900

    add: chakra-ui

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,12 +1,12 @@
 import type { AppProps } from 'next/app'
-// import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Component {...pageProps} />
-    // <ChakraProvider>
-    //   <Component {...pageProps} />
-    // </ChakraProvider>
-  )
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
-export default MyApp
+
+export default MyApp;
