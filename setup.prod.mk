ef92174cb76514615339da11ebd15fad851ed62a
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2021-10-23 11:44:09 +0900

    fix docker-compose

--- setup.prod.mk
+++ setup.prod.mk
@@ -8,7 +8,7 @@ install-tools: \
 	amazon-linux-extras install -y docker
 
 /usr/local/bin/docker-compose:
-	curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+	curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(shell uname -s)-$(shell uname -m)" -o /usr/local/bin/docker-compose
 	chmod +x /usr/local/bin/docker-compose
 
 /usr/local/bin/vim:
