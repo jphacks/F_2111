2ed99d284b28a5c6a856c11b33d333d3a92b9f34
takurinton <takurinton@takurinton.com>
2021-10-24 20:50:58 +0900

    fix: view

--- frontend/src/components/pages/photo/Map.tsx
+++ frontend/src/components/pages/photo/Map.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, ReactElement } from "react";
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import { Box } from "@chakra-ui/layout";
 
 const render = (status: Status): ReactElement => {
   if (status === Status.LOADING) return <>{status}...</>;
@@ -18,26 +19,49 @@ const MapComponent = ({
     zoom: number;
     icon: string;
   }): JSX.Element => {
-    const ref = useRef(null);
+    const mapRef = useRef(null);
+    const panoRef = useRef(null);
 
     useEffect(() => {
       // https://developers.google.com/maps/documentation/javascript/examples/marker-simple?hl=ja
       // @ts-ignore
-      const map = new window.google.maps.Map(ref.current, {
+      const map = new window.google.maps.Map(mapRef.current, {
         center,
         zoom,
       })
       // @ts-ignore
-      const marker = new window.google.maps.Marker({
+      // const marker = new window.google.maps.Marker({
+      //   position: center,
+      //   map, 
+      //   // icon
+      // });
+
+      // @ts-ignore
+      const pano = new google.maps.StreetViewPanorama(panoRef.current, {
         position: center,
-        map, 
-        icon
+        pov: {
+          heading: 34,
+          pitch: 10,
+        },
       });
 
+      map.setStreetView(pano);
     }, [center, zoom]);
-
   
-    return <div style={{ height: '50vh', width: '50vw', margin: '50px auto' }} ref={ref} id="map" />;
+    return (
+      <Box height="50vh" width="80vw" margin="50px auto">
+        <Box ref={mapRef} id="map" style={{
+          float: 'left',
+          height: '100%',
+          width: '50%',
+        }}></Box>
+        <Box ref={panoRef} id="pano" style={{
+          float: 'left',
+          height: '100%',
+          width: '50%',
+        }}></Box>
+      </Box>
+    );
   }
 
 export const Map = ({
