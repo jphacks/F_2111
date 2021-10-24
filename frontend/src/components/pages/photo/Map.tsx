fb8f451d8c0a8b9716faa1a3d89b973276aa8465
Takumi Katayama <takurinton@takurinton.com>
2021-10-24 22:43:00 +0900

    Google Maps を可愛く (#34)
    * add: icon
    
    * fix: view
    
    * Update frontend/src/components/pages/photo/Map.tsx
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update frontend/src/components/pages/photo/Map.tsx
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * fix
    
    * fix
    
    * add: window
    
    * fix
    
    * Update frontend/src/components/pages/photo/Map.tsx
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- frontend/src/components/pages/photo/Map.tsx
+++ frontend/src/components/pages/photo/Map.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, ReactElement } from "react";
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import { Box } from "@chakra-ui/layout";
 
 const render = (status: Status): ReactElement => {
   if (status === Status.LOADING) return <>{status}...</>;
@@ -11,44 +12,91 @@ const render = (status: Status): ReactElement => {
 const MapComponent = ({
     center,
     zoom,
+    icon,
+    title,
+    lat, 
+    lng,
   }: {
     // @ts-ignore
     center: google.maps.LatLngLiteral;
     zoom: number;
+    icon: string;
+    title: string;
+    lat: number;
+    lng: number;
   }): JSX.Element => {
-    const ref = useRef(null);
+    const mapRef = useRef(null);
+    const panoRef = useRef(null);
+
+    // @ts-ignore
+    const place = new google.maps.LatLng(lat, lng)
+    const createCard = () => `
+      <div>
+        <p style="padding-bottom: 5px; font-size: 1rem">${title}</p>
+        <img src="${icon}" style="height: 100px; width: 150px;"/>
+      </div>
+      `
 
     useEffect(() => {
       // https://developers.google.com/maps/documentation/javascript/examples/marker-simple?hl=ja
       // @ts-ignore
-      const map = new window.google.maps.Map(ref.current, {
+      const map = new window.google.maps.Map(mapRef.current, {
         center,
         zoom,
       })
-      // @ts-ignore
-      new window.google.maps.Marker({
-        position: center,
-        map
-      });
-    }, [center, zoom]);
 
+      // @ts-ignore
+      const pano = new google.maps.StreetViewPanorama(panoRef.current, {
+        position: center,
+        pov: {
+          heading: 34,
+          pitch: 0,
+        },
+      });
+
+      // @ts-ignore
+      const card = new google.maps.InfoWindow();
+      card.setContent(createCard());
+      card.setPosition(place);
+      card.open(map);
+
+      map.setStreetView(pano);
+    }, [center, zoom]);
   
-    return <div style={{ height: '50vh', width: '50vw', margin: '50px auto' }} ref={ref} id="map" />;
+    return (
+      <Box height="50vh" width="80vw" margin="50px auto">
+        <Box ref={mapRef} id="map" float="left" height="100%" width="50%" />
+        <Box ref={panoRef} id="pano" float="left" height="100%" width="50%" />
+      </Box>
+    );
   }
 
 export const Map = ({
-  lat, lng 
+  lat, 
+  lng,
+  image, 
+  title,
 }: {
   lat: number;
   lng: number;
+  image: string;
+  title: string;
 }): JSX.Element => {
   const API_KEY = '';
   const center = { lat, lng };
-  const zoom = 10;
+  const icon = image;
+  const zoom = 15;
 
   return (
     <Wrapper apiKey={API_KEY} render={render}>
-        <MapComponent center={center} zoom={zoom} />
+        <MapComponent 
+          center={center} 
+          zoom={zoom} 
+          icon={icon} 
+          title={title}
+          lat={lat} 
+          lng={lng}
+        />
     </Wrapper>
   )
 };
