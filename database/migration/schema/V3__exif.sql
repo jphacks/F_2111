81b4ae314fa33487553e957a29405b5a94a8a890
masimacbook <masi19bw@gmail.com>
2021-10-26 12:55:03 +0900

    fix: typo, altitude decimal

--- database/migration/schema/V3__exif.sql
+++ database/migration/schema/V3__exif.sql
@@ -16,7 +16,7 @@ ALTER TABLE photos ADD `focal_length` DECIMAL(7,2);
 ALTER TABLE photos ADD `photo_graphic_sensitivity` INT UNSIGNED;
 
 -- 露出(露光)補正値
-ALTER TALBE photos ADD `exposure_bias_value` DECIMAL(10, 7);
+ALTER TABLE photos ADD `exposure_bias_value` DECIMAL(10, 7);
 
 -- APEX値
 ALTER TABLE photos ADD `shutter_speed_value`   DECIMAL(21, 15);
@@ -29,7 +29,7 @@ ALTER TABLE photos ADD `gps_latitude`   DECIMAL(10, 7);
 -- 経度
 ALTER TABLE photos ADD `gps_longitude`   DECIMAL(10, 7);
 -- 高度
-ALTER TABLE photos ADD `gps_altitude`   INT UNSIGNED;
+ALTER TABLE photos ADD `gps_altitude`   DECIMAL(10, 7);
 
 -- T=真方位 M=磁気方位
 ALTER TABLE photos ADD `gps_img_direction_ref`   CHAR(1);
