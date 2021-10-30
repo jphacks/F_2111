c1bd0e1f76ef6e43b7605eda8d9ed6fd1ae3047a
takurinton <takurinton@takurinton.com>
2021-10-30 12:25:23 +0900

    add: kawaii

--- frontend/pages/404.tsx
+++ frontend/pages/404.tsx
@@ -1,5 +1,13 @@
+import { Box, chakra } from '@chakra-ui/react';
+import { Image } from '@chakra-ui/image';
 import React from 'react';
 
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>
+  return (
+    <Box textAlign="center" padding="100px">
+      <Image margin="0 auto 50px" src="https://baetoru.tetsuzawa.com/Logo.jpg" width="50%" />
+      <chakra.h1 fontSize="2rem" fontWeight="800">404 Page Not Found...</chakra.h1>
+      <chakra.h1 fontSize="2rem" fontWeight="800">お探しのページは見つかりませんでした。</chakra.h1>
+    </Box>
+  )
 }
